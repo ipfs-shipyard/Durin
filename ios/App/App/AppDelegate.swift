8c0d1882b69f1c6703f3e54afe3b9371db23b9e3
drewdelano <22156330+drewdelano@users.noreply.github.com>
2023-03-30 14:53:19 -0700

    add DurinShareExtension

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import CapacitorShareExtension
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -33,11 +34,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
+    //func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    //    // Called when the app was launched with a url. Feel free to add additional processing here,
+    //    // but if you want the App API to support tracking app url opens, make sure to keep this call
+    //    return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    //}
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
@@ -45,6 +46,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
+    
+    let store = ShareStore.store
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+                
+            var success = true
+            if CAPBridge.handleOpenUrl(url, options) {
+                success = ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+            }
+        
+            guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
+                  let params = components.queryItems else {
+                      return false
+                  }
+            let titles = params.filter { $0.name == "title" }
+            let descriptions = params.filter { $0.name == "description" }
+            let types = params.filter { $0.name == "type" }
+            let urls = params.filter { $0.name == "url" }
+            let webPaths = params.filter { $0.name == "webPath" }
+        
+            store.shareItems.removeAll()
+        
+            if (titles.count > 0){
+                for index in 0...titles.count-1 {
+                    var shareItem: JSObject = JSObject()
+                    shareItem["title"] = titles[index].value!
+                    shareItem["description"] = descriptions[index].value!
+                    shareItem["type"] = types[index].value!
+                    shareItem["url"] = urls[index].value!
+                    shareItem["webPath"] = webPaths[index].value!
+                    store.shareItems.append(shareItem)
+                }
+            }
+        
+            store.processed = false
+            let nc = NotificationCenter.default
+            nc.post(name: Notification.Name("triggerSendIntent"), object: nil )
+        
+            return success
+        }
 
 
 }
