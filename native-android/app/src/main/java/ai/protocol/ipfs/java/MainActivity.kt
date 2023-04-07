bee8bb22a54268b363836e40a8d472cb66524165
Jason Hudgins <jason@hudgins.co>
2023-04-07 16:31:01 -0500

    added a compose based theme

--- native-android/app/src/main/java/ai/protocol/ipfs/java/MainActivity.kt
+++ native-android/app/src/main/java/ai/protocol/ipfs/java/MainActivity.kt
@@ -1,10 +1,17 @@
 package ai.protocol.ipfs.java
 
+import ai.protocol.ipfs.java.theme.DurinTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 // TODO - understand basic theming /w compose
 
@@ -12,10 +19,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorld(name = "Jason")
+            DurinTheme {
+                // use a box to set our background image
+                Box (modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painterResource(id = R.drawable.splash),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.matchParentSize()
+                    )
+                }
+                HelloWorld(name = "Jason")
+            }
         }
     }
-
 
     @Composable
     fun HelloWorld(name : String) {
---
+++ native-android/app/src/main/java/ai/protocol/ipfs/java/theme/DurinTheme.kt
@@ -0,0 +1,10 @@
+package ai.protocol.ipfs.java.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DurinTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
