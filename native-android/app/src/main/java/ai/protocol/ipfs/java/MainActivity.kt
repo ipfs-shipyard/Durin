b5f65ea5a3e729e8ca9253c1b071fd9283b4bc10
Jason Hudgins <jason@hudgins.co>
2023-04-16 17:53:07 -0500

    MainActivity layout + tabs is very roughly correct

--- native-android/app/src/main/java/ai/protocol/ipfs/java/MainActivity.kt
+++ native-android/app/src/main/java/ai/protocol/ipfs/java/MainActivity.kt
@@ -1,16 +1,47 @@
 package ai.protocol.ipfs.java
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-
-
-// TODO - figure how to use fragments / viewpager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+    val tableIcons = arrayOf(R.drawable.search, R.drawable.cloud_upload, R.drawable.settings)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContentView(R.layout.activity_main)
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager)
+        viewPager.adapter = HomePagerAdapter(this)
+
+        // tab layout
+        tabLayout = findViewById(R.id.tablayout)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setIcon(tableIcons[position])
+        }.attach()
+    }
+
+
+    private inner class HomePagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+        override fun getItemCount(): Int {
+            return 3
         }
+
+        override fun createFragment(position: Int): Fragment {
+            return HomeFragment()
+        }
+
     }
 }
