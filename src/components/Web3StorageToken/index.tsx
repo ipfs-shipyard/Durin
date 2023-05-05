ef26cb69afdd0d4de02f7188a0f67f8da3d42fb9
drewdelano <22156330+drewdelano@users.noreply.github.com>
2023-05-04 20:55:34 -0700

    Add control for updates

--- src/components/Web3StorageToken/index.tsx
+++ src/components/Web3StorageToken/index.tsx
@@ -1,11 +1,28 @@
 import { FC } from 'react'
 import './index.scss'
+import { IonInput } from '@ionic/react'
+import createPersistedState from 'use-persisted-state'
+import { SettingsObject } from '../../util/ipfs'
 
 const Web3StorageToken: FC = () => {
+  const useSettings = createPersistedState<SettingsObject>('durin-settings')
+  const [settings, setSettings] = useSettings({
+    node: 'auto',
+    web3storageToken: ''
+  })
+
   return (
-        <div>
-          Web3.StorageToken setter goes here
-        </div>
+    <IonInput
+      type='text'
+      placeholder='Web3.Storage Token'
+      value={settings.web3storageToken}
+      className='durin-input'
+      required
+      onIonChange={(e) => setSettings({
+        ...settings,
+        web3storageToken: e.detail.value!
+      })}
+    />
   )
 }
 
