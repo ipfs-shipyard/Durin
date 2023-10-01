4bbd40bd885910aa2de030563c9982b7747034a0
Jason Hudgins <jason@hudgins.co>
2023-10-01 07:47:49 -0500

    removing Compose code / going back to standard layouts

--- native-android/app/src/main/java/ai/protocol/ipfs/filforge/HomeFragment.kt
+++ native-android/app/src/main/java/ai/protocol/ipfs/filforge/HomeFragment.kt
@@ -1,60 +1,54 @@
 package ai.protocol.ipfs.filforge
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.Text
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
+import io.ipfs.cid.Cid
 
 
 class HomeFragment : Fragment() {
-    // private var cidField: EditText? = null
-    // private var launchButton: Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        // TODO - start building compose UX
-        // TODO - find background image or color for the app (splash.png)
-        // TODO - put splash in the proper place
+        val rootView = inflater.inflate(R.layout.fragment_home, container)
+        val cidField : EditText = rootView.findViewById(R.id.field_cid)
+        val launchButton : Button = rootView.findViewById(R.id.btn_ipfs_intent)
+        launchButton.setOnClickListener {
+            val userInput = cidField.text.toString()
+            try {
+                val i = Intent()
+                i.action = Intent.ACTION_VIEW
+                val uri = Uri.parse(userInput)
+                val scheme = uri.scheme
+                if (scheme != "ipfs" && scheme != "ipns") {
+                    Toast.makeText(
+                        requireContext(),
+                        "Scheme must be ipfs:// or ipns://",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
 
-        return ComposeView(requireContext()).apply {
-            setContent {
-                HelloWorld()
+                // quick sanity check on the CID, will throw exceptions if there's an issue
+                Cid.decode(uri.authority)
+                i.data = uri
+                startActivity(i)
+            } catch (exception: RuntimeException) {
+                Toast.makeText(
+                    requireContext(),
+                    "Invalid CID: " + exception.message,
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
 
-    //
-//        val rootView = inflater.inflate(R.layout.fragment_home, container)
-//        cidField = rootView.findViewById(R.id.field_cid)
-//        launchButton = rootView.findViewById(R.id.btn_ipfs_intent)
-//        launchButton.setOnClickListener(View.OnClickListener { view: View? ->
-//            val userInput = cidField.getText().toString()
-//            try {
-//                val i = Intent()
-//                i.action = Intent.ACTION_VIEW
-//                val uri = Uri.parse(userInput)
-//                val scheme = uri.scheme
-//                if (scheme != "ipfs" && scheme != "ipns") {
-//                    Toast.makeText(requireContext(), "Scheme must be ipfs:// or ipns://", Toast.LENGTH_LONG).show()
-//                    return@setOnClickListener
-//                }
-//
-//                // quick sanity check on the CID, will throw exceptions if there's an issue
-//                Cid.decode(uri.authority)
-//                i.data = uri
-//                startActivity(i)
-//            } catch (exception: RuntimeException) {
-//                Toast.makeText(requireContext(), "Invalid CID: " + exception.message, Toast.LENGTH_LONG).show()
-//            }
-//        })
-    }
-
-    @Composable
-    fun HelloWorld() {
-        Text(text = "Hello World")
+        return rootView
     }
 
 }
