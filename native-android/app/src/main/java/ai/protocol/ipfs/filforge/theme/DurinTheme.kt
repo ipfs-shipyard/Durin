d2aa5cd0e0fce5315bcf0fda022724fb26628a4f
Jason Hudgins <jason@hudgins.co>
2023-10-06 16:39:28 -0500

    removed compose activities

--- native-android/app/src/main/java/ai/protocol/ipfs/filforge/theme/DurinTheme.kt
+++
@@ -1,9 +0,0 @@
-package ai.protocol.ipfs.filforge.theme
-
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DurinTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
-}
