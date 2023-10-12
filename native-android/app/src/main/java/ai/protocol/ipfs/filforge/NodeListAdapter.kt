d8ee1b77eae22643ecbf81545f26f5cb13cd42d0
Jason Hudgins <jason@hudgins.co>
2023-10-12 17:45:04 -0500

    initial work on the settings tab is completed

---
+++ native-android/app/src/main/java/ai/protocol/ipfs/filforge/NodeListAdapter.kt
@@ -0,0 +1,31 @@
+package ai.protocol.ipfs.filforge
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class NodeListAdapter(private val nodeList: List<Node>) : RecyclerView.Adapter<NodeListAdapter.NodeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NodeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_node, parent, false)
+        return NodeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return nodeList.size
+    }
+
+    override fun onBindViewHolder(holder: NodeViewHolder, position: Int) {
+        val currentNode = nodeList[position]
+        holder.nodeTextView.text = currentNode.host + " - " + currentNode.speed + "ms"
+    }
+
+    inner class NodeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val nodeTextView: TextView = itemView.findViewById<TextView>(R.id.node_text)
+    }
+
+
+}
--- native-android/app/src/main/java/ai/protocol/ipfs/filforge/SettingsFragment.kt
+++ native-android/app/src/main/java/ai/protocol/ipfs/filforge/SettingsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SettingsFragment : Fragment() {
 
@@ -13,6 +15,19 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val nodeListRecyclerView = view.findViewById<RecyclerView>(R.id.node_list_recycler_view)
+        nodeListRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val sortedHealthyNodes = nodeList.filter { it.healthy }.sortedBy { it.speed }
+        nodeListRecyclerView.adapter = NodeListAdapter(sortedHealthyNodes)
+    }
+
+
 }
